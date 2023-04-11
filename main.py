d071cdf0803d9b7ed2b82ddefccf42b61e964e6e
Billit Shotta <billitshotta@gmail.com>
2023-04-11 14:58:06 +0700

    get total ayat has ready by input ayat and surah

--- main.py
+++ main.py
@@ -33,33 +33,166 @@ JUZ = [
         431,
         564,
     ]
+SURAH = [
+    7, 
+    286,
+    200,
+    176,
+    120,
+    165,
+    206,
+    75,
+    129,
+    109,
+    123,
+    111,
+    43,
+    52,
+    99,
+    128,
+    111,
+    110,
+    98,
+    135,
+    112,
+    78,
+    118,
+    64,
+    77,
+    227,
+    93,
+    88,
+    69,
+    60,
+    34,
+    30,
+    73,
+    54,
+    45,
+    83,
+    182,
+    88,
+    75,
+    85,
+    54,
+    53,
+    89,
+    59,
+    37,
+    35,
+    38,
+    29,
+    18,
+    45,
+    60,
+    49,
+    62,
+    55,
+    78,
+    96,
+    29,
+    22,
+    24,
+    13,
+    14,
+    11,
+    11,
+    18,
+    12,
+    12,
+    30,
+    52,
+    52,
+    44,
+    28,
+    28,
+    20,
+    56,
+    40,
+    31,
+    50,
+    40,
+    46,
+    42,
+    29,
+    19,
+    36,
+    25,
+    22,
+    17,
+    19,
+    26,
+    30,
+    20,
+    15,
+    21,
+    11,
+    8,
+    8,
+    19,
+    5,
+    8,
+    8,
+    11,
+    11,
+    8,
+    3,
+    9,
+    5,
+    4,
+    7,
+    3,
+    6,
+    3,
+    5,
+    4,
+    5,
+    6,
+
+]
 
 def Juz(inputJuz):
     return JUZ[inputJuz-1]
 
-#input target
-inputTargetDay = int(input("Target Khatam (D) :"))
-inputTargetMon = int(input("Target Khatam (M) :"))
-inputTargetYear = int(input("Target Khatam (Y) :"))
-#print(inputTargetDay,inputTargetMon,inputTargetYear)
+# #input target
+# inputTargetDay = int(input("Target Khatam (D) :"))
+# inputTargetMon = int(input("Target Khatam (M) :"))
+# inputTargetYear = int(input("Target Khatam (Y) :"))
+# #print(inputTargetDay,inputTargetMon,inputTargetYear)
 
-targetKhatam = datetime.datetime(inputTargetYear,inputTargetMon,inputTargetDay)
-print(targetKhatam.strftime("%d %m %Y"))
+# targetKhatam = datetime.datetime(inputTargetYear,inputTargetMon,inputTargetDay)
+# print(targetKhatam.strftime("%d %m %Y"))
 
-#input start date
-inputStartDay = int(input("Start Read (D) :"))
-inputStartMon = int(input("Start Read (M) :"))
-inputStartYear = int(input("Start Read (Y) :"))
-#print(inputStartDay,inputStartMon,inputStartYear)
+# #input start date
+# inputStartDay = int(input("Start Read (D) :"))
+# inputStartMon = int(input("Start Read (M) :"))
+# inputStartYear = int(input("Start Read (Y) :"))
+# #print(inputStartDay,inputStartMon,inputStartYear)
 
-#now = datetime.datetime.now()
-startDate = datetime.datetime(inputStartYear,inputStartMon,inputStartDay)
-#print(now.strftime("%d %m %Y"))
-print(startDate.strftime("%d %m %Y"))
+# #now = datetime.datetime.now()
+# startDate = datetime.datetime(inputStartYear,inputStartMon,inputStartDay)
+# #print(now.strftime("%d %m %Y"))
+# print(startDate.strftime("%d %m %Y"))
 
-#calcuating days left
-resultDay=targetKhatam - startDate
-print("{} Hari lagi".format(resultDay.days))
+# #calcuating days left
+# resultDay=targetKhatam - startDate
+# print("{} Hari lagi".format(resultDay.days))
 
-#last read
-lastRead = int(input("Last read (juz):"))
+#input last read
+lastReadAyat = int(input("Last Read (ayat):")) # ayat ke 2
+lastReadSurah = int(input("Last Read (surah):")) #surah ke 2
+
+#calculating total surah has read
+#Get total ayat in surah has read
+totalAyat = 0
+for i in range(lastReadSurah-1):
+    #print(i, SURAH[i])
+    totalAyat += SURAH[i]
+    #print("Total",totalAyat)
+
+# jumlah nya di tambah dengan ayat terakhir
+totalAyat += lastReadAyat
+print("Total ayat : ",totalAyat)
+
+
+
